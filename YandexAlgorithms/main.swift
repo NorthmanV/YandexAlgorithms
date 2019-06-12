0fc660188e5715931280e60575aab640ca4adfed
Ruslan Akberov <ruslanakberov@gmail.com>
2019-06-12 16:37:20 +0300

    Initial Commit

---
+++ YandexAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YandexAlgorithms
+//
+//  Created by Ruslan Akberov on 12/06/2019.
+//  Copyright © 2019 Ruslan Akberov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
